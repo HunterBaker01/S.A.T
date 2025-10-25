18ee2dd1b5beb90d07a8dc0c4233d80d6b39f3dc
Hunter Baker <hbaker599@gmail.com>
2025-10-25 14:31:55 +0200

    Flickr8k dataset pipeline (hope it works)

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -1,6 +1,7 @@
 import torchvision.transforms as transforms
-from torchvision.datasets import Flickr8k
-from torch.utils.data import random_split
+import os
+import torch
+from PIL import Image
 
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -12,19 +13,43 @@ transform = transforms.Compose([
     ),
 ])
 
-def get_data(root, annotations_file, transform=transform, train_size=0.8):
-    dataset = Flickr8k(
-        root=root,
-        ann_file=annotations_file,
-        transform=transform
-    )
-    
-    train_size = int(train_size * len(dataset))
-    test_size = len(dataset) - train_size
+class Flickr8kDataset(Dataset):
+    def __init__(self, imgid2captions, vocab, transform=transform):
+        self.imgid2captions = []
+        self.transform = transform
+        self.vocab = vocab
 
-    train_dataset, test_dataset = random_split(
-        dataset,
-        [train_size, test_size]
-    )
+        for img_id, caps in imgid2captions.items():
+            for c in caps:
+                self.imgid2captions.append((img_id, c))
 
-    return train_dataset, test_dataset
+    def __len__(self):
+        return len(self.imgid2captions)
+
+    def __getitem__(self, idx):
+        img_id, caption = self.imgid2captions[idx]
+        img_path = os.path.join(IMGAGES_DIR, img_id)
+        image = Image.open(img_path).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
+
+        numerical_caption = [self.vocab.ind["sos"]]
+        numerical_caption += self.vocab.numerical(caption)
+        numerical_caption.append(self.vocab.ind["eos"])
+
+        return image, torch.tensor(numerical_caption, dtype=torch.long)
+
+    def collate_fn(batch):
+        batch.sort(key=lambda x: len(x[1]), reverse=True)
+        images = [item[0] for item in batch]
+        captions = [item[1] for item in batch]
+        lengths = [len(cap) for cap in captions]
+        max_len = max(lengths)
+
+        padded_captions = torch.zeros(len(captions), max_len, dtype=torch.long)
+        for i, cap in enumerate(captions):
+            end = lengths[i]
+            padded_captions[i, :end] = cap[:end]
+
+        images = torch.stack(images, dim=0)
+        return images, padded_captions, lengths
